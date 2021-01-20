48513db02f92afc665e19a98e7078920358a87a8
Naeem <naeemh93@gmail.com>
2021-01-20 17:02:29 +0500

    code indentation refactoring.

--- meeting.rb
+++ meeting.rb
@@ -1,89 +1,76 @@
 
-class Meeting
+  class Meeting
+    def initialize(appointments)
+      @appointments = appointments
+      time_slots
+    end
 
+    def validate_meeting
+      @appointments.sort_by { |appt| appt[:type] }
+      padding_appointments
+      @appointments.each do |appointment|
+        success = book_slot(appointment)
+        if !success
+          return 'No Cant Fit'
+        end
+      end
+      'Yes Can Fit'
+    end
 
-  def initialize(appointments)
-    @appointments = appointments
-    time_slots
-  end
+    def show
+      return 'There are no appointments.' if @appointments.nil?
+      result = ''
+      filled_slots.group_by { |s| s[:slot_id] }.each do |key, value|
+        appt = @appointments.find { |a| a[:id] == value.first[:slot_id] }
+        result << "#{value.first[:starttime]} - #{value.last[:endtime]} - #{appt[:name]}\n"
+      end
+      result + "\n"
+    end
 
-  def validate_meeting
-     @appointments.sort_by { |appt| appt[:type] }
+    private
 
-    padding_appointments
-    @appointments.each do |appointment|
-      success = book_slot(appointment)
-      if !success
-        return false
+    def time_slots
+      require 'time'
+      @time_slots= []
+      newdatetime=[]
+      (0..15).each_with_index do |a,i|
+        thirty_minutes_step = (1.to_f/24/2)
+        date_time = DateTime.parse('9:00 AM')
+        date_time_limit = DateTime.parse('5:00 PM')
+        date_time.step(date_time_limit,thirty_minutes_step).each{|e| newdatetime <<  e.strftime("%I:%M %p")}
+        @time_slots<< {:id=> i, :starttime=> "#{newdatetime[i]}", :endtime=> "#{newdatetime[i+1]}", :slot_id=> nil }
       end
     end
-    true
-  end
 
-  def show
-    return 'There are no appointments.' if @appointments.nil?
-
-    result = ''
-    filled_slots.group_by { |s| s[:slot_id] }.each do |key, value|
-      appt = @appointments.find { |a| a[:id] == value.first[:slot_id] }
-      result << "#{value.first[:start]} - #{value.last[:end]} - #{appt[:name]}\n"
+    def padding_appointments
+      @appointments.each { |appointment|  appointment[:padding] = appointment[:type] == :offsite ? 0.5 : 0 }
+      @appointments.last[:padding] = 0
     end
-    result + "\n"
-  end
 
-
-
-  private
-
-
-  def time_slots
-    require 'time'
-    @time_slots= []
-    newdatetime=[]
-    (0..15).each_with_index do |a,i|
-      thirty_minutes_step = (1.to_f/24/2)
-      date_time = DateTime.parse('9:00 AM')
-      date_time_limit = DateTime.parse('5:00 PM')
-
-      date_time.step(date_time_limit,thirty_minutes_step).each{|e| newdatetime <<  e.strftime("%I:%M %p")}
-
-      @time_slots<< {:id=> i, :starttime=> "#{newdatetime[i]}", :endtime=> "#{newdatetime[i+1]}", :slot_id=> nil }
+    def book_slot(appointment)
+      no_of_slots = ((appointment[:duration] + appointment[:padding]) / 0.5).to_i
+      if  empty_slots.length >= no_of_slots
+        fill_slots(no_of_slots, appointment[:id])
+        true
+      else
+        false
+      end
     end
-    
-  end
 
-
-  def padding_appointments
-    @appointments.each { |appointment|  appointment[:padding] = appointment[:type] == :offsite ? 0.5 : 0 }
-    @appointments.last[:padding] = 0
-
-  end
-
-  def book_slot(appointment)
-    no_of_slots = ((appointment[:duration] + appointment[:padding]) / 0.5).to_i
-    if  empty_slots.length >= no_of_slots
-      fill_slots(no_of_slots, appointment[:id])
-      true
-    else
-      false
+    def fill_slots(number_of_slots, appt_id)
+      start_index = empty_slots.first[:id]
+      number_of_slots.times { |i|  @time_slots[start_index + i][:slot_id] = appt_id  }
     end
+
+    def empty_slots
+       @time_slots.select{|s| s[:slot_id].nil?}
+    end
+
+    def filled_slots
+      @time_slots.reject { |s| s[:slot_id].nil? }
+    end
+
   end
-
-  def fill_slots(number_of_slots, appt_id)
-    start_index = empty_slots.first[:id]
-
-    number_of_slots.times { |i|  @time_slots[start_index + i][:slot_id] = appt_id  }
-  end
-
-  def empty_slots
-     @time_slots.select{|s| s[:slot_id].nil?}
-  end
-
-  def filled_slots
-    @time_slots.reject { |s| s[:slot_id].nil? }
-  end
-
-end
 
 
 
--- spec/meeting_spec.rb
+++ spec/meeting_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Meeting do
     it 'should return true for meetings  that are fit ' do
       appointments = [{ id: 1, name: 'Meeting 1', duration: 1, type: :onsite }]
       schedule = Meeting.new(appointments)
-      expect(schedule.validate_meeting).to eq true
+      expect(schedule.validate_meeting).to eq 'Yes Can Fit'
     end
 
     it 'should return false for meetings that are not  fit' do
       appointments = []
       schedule = Meeting.new(appointments)
       9.times { |i| appointments << { id: i, name: "Meeting #{i}", duration: 1, type: :onsite } }
-      expect(schedule.validate_meeting).to eq false
+      expect(schedule.validate_meeting).to eq 'No Cant Fit'
     end
 
     it 'should handle padding for offsite appointments' do
@@ -23,20 +23,34 @@ RSpec.describe Meeting do
         { id: 2, name: 'Meeting 2', duration: 4, type: :offsite }
       ]
       schedule = Meeting.new(appointments)
-      expect(schedule.validate_meeting).to eq false
+      expect(schedule.validate_meeting).to eq 'No Cant Fit'
     end
 
   end
 
   context 'passing example test' do
-    it 'Given a set of meetings (below is an example of a set of meetings):' do
+
+    it 'Given a set of meetings & return true if they fit' do
       appointments = [
-        { id: 1, name: 'Meeting 1', duration: 3, type: :onsite },
+        { id: 1, name: 'Meeting 1', duration: 1.5, type: :onsite },
         { id: 2, name: 'Meeting 2', duration: 2, type: :offsite },
-        { id:3, name:  'Meeting 3', duration: 1,  type: :offsite}
+        { id: 3, name: 'Meeting 3', duration: 1, type: :onsite },
+        { id: 4, name: 'Meeting 4', duration: 1, type: :offsite },
+        { id: 5, name: 'Meeting 5', duration: 1, type: :offsite }
       ]
       schedule = Meeting.new(appointments)
-      expect(schedule.validate_meeting).to eq true
+      expect(schedule.validate_meeting).to eq 'Yes Can Fit'
+      expect(schedule.show).to eq "09:00 AM - 10:30 AM - Meeting 1\n10:30 AM - 01:00 PM - Meeting 2\n01:00 PM - 02:00 PM - Meeting 3\n02:00 PM - 03:30 PM - Meeting 4\n03:30 PM - 04:30 PM - Meeting 5\n\n"
+      
+    end
+
+    it 'Given a set of meeting for onsite:' do
+      appointments = [
+        { id: 1, name: 'Meeting 1', duration: 3, type: :onsite },
+      ]
+      schedule = Meeting.new(appointments)
+      expect(schedule.validate_meeting).to eq 'Yes Can Fit'
+      expect(schedule.show).to eq "09:00 AM - 12:00 PM - Meeting 1\n\n"
     end
 
     it 'Given set of meetings for  offsite' do
@@ -45,7 +59,7 @@ RSpec.describe Meeting do
         { id: 2, name: 'Meeting 2', duration: 4, type: :offsite }
       ]
       schedule = Meeting.new(appointments)
-      expect(schedule.validate_meeting).to eq false
+      expect(schedule.validate_meeting).to eq 'No Cant Fit'
     end
 
   end
